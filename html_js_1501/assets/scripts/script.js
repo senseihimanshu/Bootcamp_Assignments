3a00ee50b3dff67568b775e3d98a1956a169ef31
= <=>
2020-01-15 22:50:57 +0530

    completed add list items functionality as per 15jan

---
+++ html_js_1501/assets/scripts/script.js
@@ -0,0 +1,17 @@
+function createItem(){
+    const inputBoxContent = document.querySelector('.input-box').value;
+    if(inputBoxContent !== ''){
+        const newLi = document.createElement('li');
+        newLi.textContent = inputBoxContent;
+
+        document.querySelector('.list-parent').appendChild(newLi);
+
+        //Emptying the input box
+        document.querySelector('.input-box').value = '';
+    }else{
+        setInterval(()=>{
+            document.querySelector('.msg').classList.add('msg--hidden');
+        }, 2000);
+        document.querySelector('.msg').classList.remove('msg--hidden');
+    }
+}
